38ada32d4d96be8591367b63e71799e6f1631b58
Queren Silva <qjdsilva@gmail.com>
2024-03-10 21:23:59 -0300

    API project created

---
+++ SistemaTarefas/Models/TarefaModel.cs
@@ -0,0 +1,16 @@
+﻿
+using SistemaTarefas.Enums;
+
+namespace SistemaTarefas.Models
+{
+    public class TarefaModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public StatusTarefa Status { get; set; }
+
+
+
+    }
+}
